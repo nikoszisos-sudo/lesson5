9e6159de4ab42b1bfbbfd9d0d40b124bd8867c27
Nikolaos Zisos <nikoszisos@gmail.com>
2026-02-03 22:13:15 +0200

    ok

---
+++ tetragona_arithmon.py
@@ -0,0 +1,7 @@
+eisodos = input("Δώσε ένα αριθμό ή γράψε έξοδος: ")
+while eisodos != "έξοδος":
+    print (int(eisodos)*int(eisodos))
+    eisodos = input("Δώσε ένα αριθμό ή γράψε έξοδος: ")
+else:
+    print ("Αντίο")
+

793f8a11bb7d0a5c7b977ca5f70d4d3e128da22f
Nikolaos Zisos <nikoszisos@gmail.com>
2026-02-03 22:13:31 +0200

    ok

---
+++ athrisma_arithmon.py
@@ -0,0 +1,8 @@
+metritis = 1
+athroisma = 0
+while (metritis <= 5):
+    timi = int(input("Δωσε τον " + str(metritis) + "ο αριθμό: "))
+    metritis = metritis + 1
+    athroisma = athroisma + timi
+print ("Το άθροισμα όλων των αριθμών είναι: " + str(athroisma))
+

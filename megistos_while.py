e6339e1397e1a39269d39f83b93f16d7bd789a88
Nikolaos Zisos <nikoszisos@gmail.com>
2026-02-06 19:54:41 +0200

    ok

--- megistos_while.py
+++ megistos_while.py
@@ -1,8 +1,8 @@
-my_list = [20,30,40,5,5,6]
+my_list = [20,30,40,555,5,6]
 metritis = 0
-megistos = my_list[0]
+megistos = my_list[metritis]
 while metritis < len(my_list):
     if my_list[metritis] > megistos:
-        megistos = my_list[metritis]
-        metritis = metritis + 1
+       megistos = my_list[metritis]
+    metritis = metritis + 1
 print ("Το άθροισμα όλων των αριθμών είναι: " + str(megistos))

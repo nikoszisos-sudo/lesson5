9250d4eb5b09b9916bc5afe5dfce9bba27309f93
Nikolaos Zisos <nikoszisos@gmail.com>
2026-02-15 21:08:06 +0200

    ok

--- eisagogi_akeraiwn_kai_prosthiki_se_lista.py
+++ eisagogi_akeraiwn_kai_prosthiki_se_lista.py
@@ -1,5 +1,9 @@
 my_list = []
-for i in range(3):
-    x = input ("Δώσε τον " + str(i+1) + "ο ακέραιο: ")
-    my_list.append(x)
+N = int(input("Δώσε αριθμό μεγαλύτερο του 3 και μικρότερο του 20: "))
+while N < 3 or N > 20:
+    N = int(input("Δώσε αριθμό μεγαλύτερο του 3 και μικρότερο του 20: "))
+for i in range(0, N):
+    N = input ("Δώσε τον " + str(i+1) + "ο ακέραιο: ")
+    my_list.append(N)
+    my_list.sort()
 print(my_list)

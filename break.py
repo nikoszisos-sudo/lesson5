20d749059061e314299473cb3946fb404b29c61a
Nikolaos Zisos <nikoszisos@gmail.com>
2026-02-18 20:47:59 +0200

    ok

---
+++ break.py
@@ -0,0 +1,11 @@
+arithmoi = [0,34,7,6,22,3,2]
+search = 6
+for arithmos in arithmoi:
+    print(arithmos)
+    if search == arithmos:
+        print ("το βρήκες")
+        break
+print ("-------------------")
+if 6 in arithmoi:
+    print ("το βρήκες")
+    
---
+++ continue.py
@@ -0,0 +1,4 @@
+for number in range(0, 10):
+    if number % 2 == 0:
+        continue
+    print (number)
